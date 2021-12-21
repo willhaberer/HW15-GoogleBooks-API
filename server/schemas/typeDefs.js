4678073783106d0351e8abb7646cd541afdb61ba
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-20 19:30:43 -0500

    added books to typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,16 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    savedBooks: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 `;
 
